79e5fe8c4478e05fe1f61c3213bce809db7466fa
johnnyliang213 <zuyanliang@gmail.com>
2023-04-29 17:34:19 -0400

    update contracts

--- src/PropertyOwnerListing.sol
+++ src/PropertyOwnerListing.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "./interface/IPropertyOwnerListing.sol";
+
+import "./ERC4907.sol";
 
 import {DataTypes} from "./libs/DataTypes.sol";
 import {Events} from "./libs/Events.sol";
@@ -14,18 +14,21 @@ import {Events} from "./libs/Events.sol";
 // it allows renter to burn it
 //
 
-contract PropertyOwnerListing is
-    IPropertyOwnerListing,
-    ERC721URIStorage,
-    Ownable
-{
+contract PropertyOwnerListing is IPropertyOwnerListing, ERC4907, Ownable {
     // Keep track of tokenId;
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
     mapping(uint256 => DataTypes.Property) public listingProperties;
 
-    constructor() ERC721("NFT BNB", "NFTBNB") {}
+    constructor() ERC4907("NFT BNB", "NFTBNB") {}
+
+    modifier onlyPropertyOwner(uint256 _propertyId, address _propertyOwner) {
+        DataTypes.Property memory property = listingProperties[_propertyId];
+        address owner = property.ownerAddress;
+        require(owner == _propertyOwner, "No able to remove property listing");
+        _;
+    }
 
     function listingProperty(
         uint256 _pricePerDay,
@@ -41,5 +44,11 @@ contract PropertyOwnerListing is
         _setTokenURI(_newPropertyId, _propertUri);
         emit Events.NewRentalListing(msg.sender, _newPropertyId);
         return _newPropertyId;
+    }
+
+    function burn(
+        uint256 _propertyId
+    ) external onlyPropertyOwner(_propertyId, msg.sender) {
+        super._burn(_propertyId);
     }
 }
--- src/libs/DataTypes.sol
+++ src/libs/DataTypes.sol
@@ -3,15 +3,9 @@
 pragma solidity ^0.8.13;
 
 library DataTypes {
-    /**
-     * @notice A struct associate with a renter.
-     *
-     * @param renterAddress The address for renter for the rental property.
-     * @param expiresAt Expires time for the renter's NFT
-     */
     struct Renter {
-        address renterAddress;
-        uint256 expiresAt;
+        address renter; // address of renter
+        uint64 expires; // unix timestamp, rent expires
     }
 
     struct Property {
